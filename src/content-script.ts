fb42e5302136cc2b11bc18289af9f5047185f55f
hal <ksrghal@gmail.com>
2024-03-31 10:00:01 +0900

    コメント追加

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,6 +1,9 @@
 
 import { ContentScriptActionFactory } from "./content-script-action-factory";
 
+/*
+    各ページでやることが微妙に異なるので、actionそれ自体をクラス化して対処
+*/
 const actionFactory = new ContentScriptActionFactory();
 const action        = actionFactory.createContentScriptAction(window.location.toString());
 

37c8f2e807ffe71e7867225e4129788c142124d7
hal <ksrghal@gmail.com>
2024-04-07 04:55:52 +0900

    Update comment

--- src/content-script-action/for-other-page.ts
+++ src/content-script-action/for-other-page.ts
@@ -4,12 +4,6 @@ import { MessageFactory } from "../message-factory";
 
 export class ContentScriptActionForOtherPage implements ContentScriptAction {
     public setup(): void { 
-        /*
-            このクラスの存在意義にも関わるが、非置換対象のページであってもbackground scriptから
-            置換実行をメッセージが送られてくる。このメッセージを受け取らないとエラーが五月蠅い
-            
-            なのでmessageを受け取り破棄するためだけの処理があると都合がよい
-        */
         chrome.runtime.onMessage.addListener((
             message      : any,
             messageSender: chrome.runtime.MessageSender,

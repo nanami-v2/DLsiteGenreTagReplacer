f7530472399e402476d7be9e2d504b9eb88c53cb
nanami-v2 <49963032+nanami-v2@users.noreply.github.com>
2024-04-07 05:29:40 +0900

    Feature/update comment (#45)

--- src/content-script-action/for-other-page.ts
+++ src/content-script-action/for-other-page.ts
@@ -5,10 +5,12 @@ import { MessageFactory } from "../message-factory";
 export class ContentScriptActionForOtherPage implements ContentScriptAction {
     public setup(): void { 
         /*
-            このクラスの存在意義にも関わるが、非置換対象のページであってもbackground scriptから
-            置換実行をメッセージが送られてくる。このメッセージを受け取らないとエラーが五月蠅い
+            実は非置換対象のページであってもbackground scriptからメッセージが送られてくる場合がある。
+            何故なら、background script側ではタブIDで判別しているから。
+            つまり置換対象ページから非置換対象ページへと同一タブで遷移した場合にメッセージが送られてくる。
             
-            なのでmessageを受け取り破棄するためだけの処理があると都合がよい
+            なのでmessageを受け取り破棄するためだけの処理があると都合がよい。
+            ――というかそうしないとエラーになる。なので現状この「何もしない」クラスは必須。
         */
         chrome.runtime.onMessage.addListener((
             message      : any,
@@ -22,7 +24,7 @@ export class ContentScriptActionForOtherPage implements ContentScriptAction {
         */
         const msgFactory = new MessageFactory();
         const msgEvent   = msgFactory.createMessageContentScriptSetuppedEvent();
-
+        
         chrome.runtime
         .sendMessage(msgEvent)
         .catch((err) => console.error(err));
